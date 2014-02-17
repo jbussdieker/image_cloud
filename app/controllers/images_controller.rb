534249c60c940e427ac36f965e5048986600fd66
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-02-17 09:20:49 -0800

    Simple image attachment

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -69,6 +69,6 @@ class ImagesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def image_params
-      params.require(:image).permit(:name, :taken_at, :longitude, :latitude)
+      params.require(:image).permit(:name, :taken_at, :longitude, :latitude, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140217163802) do
+ActiveRecord::Schema.define(version: 20140217170720) do
 
   create_table "images", force: true do |t|
     t.string   "name"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20140217163802) do
     t.string   "latitude"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
