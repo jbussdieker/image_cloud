a30126f186bb1e9f53a8e62f515582fb819fb4b0
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-02-17 10:18:18 -0800

    Some bootstrap and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ImageCloud::Application.routes.draw do
   resources :images
 
+  root 'images#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
