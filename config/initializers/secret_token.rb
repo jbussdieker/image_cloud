c51a7ca40e183330f8fdb128e6a76e30fe3af890
Joshua B. Bussdieker <jbussdieker@gmail.com>
2014-02-17 07:59:15 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ImageCloud::Application.config.secret_key_base = 'd961dc8c9d2876c3d9cea43500ad94b4024f309a69ecb445b4f2fe7ca90367c1d3744451e9f701c0234a6529551df9f91c43710738a69a8755e34caf29d7bb0a'
